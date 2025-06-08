c88c3c5ced422ac94e10409e1fff80a4c347c626
Artem <artsefimov@gmail.com>
2025-06-08 14:35:45 +0300

    db

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
@@ -23,8 +24,34 @@ type Config struct {
 }
 
 func NewConfig() *Config {
+	envLoadErr := godotenv.Load("../../.env")
+	if envLoadErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	// DB
+	driver := os.Getenv("DB_DRIVER")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	ssl := os.Getenv("DB_SSL")
+
 	return &Config{
+		Port:        port,
 		InfoLogger:  log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime),
 		DebugLogger: log.New(os.Stdout, "[DEBUG]\t", log.Ldate|log.Ltime),
+		Database: DB{
+			Driver:   driver,
+			User:     user,
+			Password: password,
+			Host:     host,
+			Port:     dbPort,
+			Name:     dbName,
+			SSL:      ssl,
+		},
 	}
 }
